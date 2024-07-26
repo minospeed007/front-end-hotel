36d1f3aa71d755390ec570da980f018f94fd85c7
minospeed007 <nwannemino@gmail.com>
2024-07-26 16:53:29 +0000

    Update FeaturedProperties.jsx

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -4,7 +4,7 @@ import useFetch from '../hooks/useFetch'
 import axios from 'axios'
 
 const FeaturedProperties = () => {
- const {data,loading,error}=useFetch('http://localhost:5000/api/hotels?featured=true&limit=4')
+ const {data,loading,error}=useFetch('https://planettour.onrender.com/api/hotels?featured=true&limit=4')
  console.log(data, 'properties');
   return (
     <div className="lContainers">
